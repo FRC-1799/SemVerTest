fb9c891342872ba2ec2de5b54501b2cb09d41d01
ThePrinterFRC1799 <frc1799printer@gmail.com>
2026-02-06 16:13:32 -0700

    commit

--- src/main/java/org/frc1799/Main.java
+++ src/main/java/org/frc1799/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Howdy!!\n");
+        System.out.print("Howdy!!!\n");
         System.out.printf("Version: %s Branch: %s\n", RobotVersion.Version, RobotVersion.Branch);
     }
 }
